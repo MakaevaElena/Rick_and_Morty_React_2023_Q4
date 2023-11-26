cc951bed363609dbe23da002c23b1039aa541f20
Makaeva Elena <yamaslena@yandex.ru>
2023-11-26 21:09:55 +0700

    feat: change 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,7 @@
 import PageNotFound from '@/Components/PageNotFound/PageNotFound';
-import Searching from '../Components/Searching/Searching';
 
 const Page404: React.FC = () => {
-  return (
-    <>
-      <Searching />
-      <PageNotFound />
-      <h1>Page Not Found</h1>
-    </>
-  );
+  return <PageNotFound />;
 };
 
 export default Page404;
